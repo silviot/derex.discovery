bcac3890d70a75c25d2d73b85da9e13c6b669491
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-02-28 16:29:52 +0100

    Update default settings for example projects

--- tests/fixtures/minimal/settings/base.py
+++ tests/fixtures/minimal/settings/base.py
@@ -1,5 +1,5 @@
 from .derex import *
 
-FEATURES['ENABLE_OAUTH2_PROVIDER'] = True
+FEATURES["ENABLE_OAUTH2_PROVIDER"] = True
 
-OAUTH_OIDC_ISSUER = 'http://lms.localhost:4700/oauth2'
+OAUTH_OIDC_ISSUER = "http://lms.localhost:4700/oauth2"
